ccb57cc43c27afba356550f01d1ec78091da6a3e
Aman <rumman.djuice1@gmail.com>
2019-07-20 01:39:59 +0200

    "Observable, Observer, subscription, Subject, map, filter, debounceTime"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+import Observable1St from './components/observable1st'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {from} from 'rxjs';//It converts static deta into Observables
+import {map, filter, scan} from 'rxjs/operators';//
+
+class App extends Component {
+
+  state={
+     numbers:[1,2,3,4,5,6,7,8,9,10],
+     result:''
+  };
+
+  clearResult=()=>{
+    this.setState({result:''});
+  };
+  filterArray=()=>{
+    //const result=this.state.numbers.map(number=>({x:number})).filter(obj=>obj.x<7);
+      let result=[];
+      from(this.state.numbers).pipe(map(number=>({x:number})),filter(obj=>obj.x<7),scan((prev, current)=>prev.concat(current), [])).subscribe(number=>result=number);//The "form()" will change the static array into Observables. The "pipe()" is used to manipulate the result before it goes to "subscription"
+      this.setState({result:JSON.stringify(result)});
+  };
+  interval=()=>{
+    let number=0;
+    const numbersArray=[];
+    const myInterval=setInterval(()=>{
+        if(number<7){
+          const obj={x:number}
+          numbersArray.push(obj);
+          number++;
+        }else {
+          clearInterval(myInterval);
+        }
+        this.setState({result:JSON.stringify(numbersArray)});
+    },1000);
+  };
+  ajaxRequest=()=>{
+      axios.get('http://api.icndb.com/jokes/random/').then(res=>{
+          return {x:res.data.value.joke.length}
+      }).then(jokeLenObj=>{
+           if(jokeLenObj.x<75){
+               this.setState({result:JSON.stringify(jokeLenObj)});
+           }
+      }).catch(err=>console.log(err))
+  };
+
+  render() {
+    return (
+        <div>
+           <button onClick={this.clearResult}>Clear</button>
+           <button onClick={this.filterArray}>Filter Array</button>
+           <button onClick={this.interval}>Interval print</button>
+           <button onClick={this.ajaxRequest}>Ajax Request</button>
+           <hr/>
+           <p>Result:{this.state.result}</p>
+           <Observable1St />
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/observable1st.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import {Observable, Observer, interval, Subject} from 'rxjs';//It converts static deta into Observables
+import {map, filter, scan, throttleTime, debounceTime} from 'rxjs/operators';//
+
+class Observable1St extends Component {
+
+    constructor(props){
+        super(props);
+        this.subject=new Subject();// so that we can use debounceTime
+    }
+
+    my1stObservable=()=>{
+        let myObservable=Observable.create((observer)=>{
+            observer.next('i am observer next()');
+
+            setTimeout(()=>{
+               observer.complete();
+            },2000);
+
+            observer.next('i am 2nd observer next()');
+
+            //observer.error('i am an error()');
+
+        });
+        myObservable.subscribe(
+            (value)=>{console.log('next():',value)},
+            (error)=>{console.log('error():',error)},
+            ()=>{console.log('complete()')},//cannot take argument
+        )
+    };
+    myIntervalObservable=()=>{
+        //let observable=Observable.interval(1000);//it will give a assending number of integer every 1 seconds
+        let observable=interval(1000);
+        observable.pipe(map(val=>val*2),throttleTime(2000)).subscribe((value)=>{console.log(value)});
+    };
+    mySubject=()=>{
+        let subject=new Subject();
+
+        subject.subscribe(
+            (value)=>{console.log('next():',value)},
+            (error)=>{console.log('error():',error)},
+            ()=>{console.log('complete()')} );//cannot take argument
+
+        subject.subscribe((value)=>{console.log('next():',value)}); //more than one subscription possible
+
+        //Now manually emit a event using subject
+        subject.next('a subject demo with next()');
+        subject.error('a subject demo with error()');
+        subject.complete();
+        subject.next('after complete'); //it will not work as it has already called complete()
+    };
+    myFilter=()=>{
+        let observable=interval(500);
+        observable.pipe(map(val=>val*2),filter(val=>val%2===0)).subscribe((value)=>{console.log(value)});
+    };
+    onInputDebounceTime=(event)=>{
+        //let subject=new Subject();// It need to be creatred inside the constructor like this.subject=new Subject// or else on each new key strock a new Subject instance will be created
+        this.subject.next(event.target.value);
+        this.subject.pipe(debounceTime(2000)).subscribe( //the "debounceTime(milisoconds)" will only emit the event after a certain time so that the activity inside "next()" such sending an http request don't happen on each key strock.
+            (value)=>{console.log('with debounceTime() next():',value)},
+            (error)=>{console.log('with debounceTime() error():',error)},
+            ()=>{console.log('complete()')},//cannot take argument
+        )
+    };
+
+    render() {
+        //this.my1stObservable();
+        //this.myIntervalObservable();
+        //this.mySubject();
+        //this.myFilter();
+        //this.onInputDebounceTime();
+        return (
+            <div>
+                <input type="text" onChange={(e)=>this.onInputDebounceTime(e)}/>
+            </div>
+        );
+    }
+}
+
+export default Observable1St;
