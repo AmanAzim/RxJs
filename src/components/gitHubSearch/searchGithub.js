b61fce8984412b55249efdd3aa157c776fccb7be
Aman <rumman.djuice1@gmail.com>
2019-07-22 18:22:49 +0200

    "repeated Ajax request with in a time interval"

--- src/components/gitHubSearch/searchGithub.js
+++ src/components/gitHubSearch/searchGithub.js
@@ -1,13 +1,14 @@
 import React,{useState} from 'react';
-import {Observable} from 'rxjs';//It converts static deta into Observables
-import {debounceTime, distinctUntilChanged, mergeMap, tap} from 'rxjs/operators';//
+import {Observable, timer} from 'rxjs';//It converts static deta into Observables
+import {debounceTime, distinctUntilChanged, mergeMap, tap, filter, switchMap, catchError} from 'rxjs/operators';//
 import {ajax} from 'rxjs/ajax'
 
 import GithubProfiles from './gitHubProfiles'
 
 const SearchGithub = () => {
 
-    const [githubData, setGithubData]=useState([])
+    const [githubData, setGithubData]=useState([]);
+    const [jokes, setJokes]=useState([]);
 
     const onSearch=(event)=>{
         let ajaxReq=Observable.create(observer=>{
@@ -16,9 +17,17 @@ const SearchGithub = () => {
             debounceTime(5000),
             distinctUntilChanged(),
             tap(()=>setGithubData([])),
+            filter(user=>!!user),
             mergeMap(user=>ajax.getJSON(`https://api.github.com/search/users?q=${user}`))
         )
         ajaxReq.subscribe((res)=>{console.log(res); setGithubData(res.items)}, (err)=>console.log(err), ()=>console.log('complete'))
+    };
+
+    const pullJokes=()=>{
+        const url='http://api.icndb.com/jokes/random/5';
+        timer(0, 5000).pipe(switchMap(()=>{
+            return ajax.getJSON(url).pipe(catchError((err)=>`An errror occured ${err}`))
+        })).subscribe((res)=>{console.log(res.value); setJokes(res.value)}, (err)=>console.log(err), ()=>console.log('complete'))
     };
 
 
@@ -36,6 +45,12 @@ const SearchGithub = () => {
                 <div className="row">
                     {githubData.map(userData=><GithubProfiles key={userData.id} userData={userData}/>)}
                 </div>
+            </div>
+            <div>
+                <button className="btn btn-primary" onClick={pullJokes}>Pull Chucknoris Jokes</button>
+                <ul>
+                    {jokes.map(value=><li key={value.id}>{value.joke}</li>)}
+                </ul>
             </div>
         </div>
     );
