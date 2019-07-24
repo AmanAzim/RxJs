118cfa965bbcccd3f9f7b2aa4154379f81afb1f5
Aman <rumman.djuice1@gmail.com>
2019-07-24 20:43:52 +0200

    "testing operator with rajs-marbles"

--- src/components/testMarble.test.js
+++ src/components/testMarble.test.js
@@ -1,28 +1,16 @@
 import { TestScheduler } from 'rxjs/testing';
 import {map,} from 'rxjs/operators';//
-import { } from 'rxjs-marbles';
+import { marbles } from "rxjs-marbles/mocha";
 
-const testScheduler = new TestScheduler((actual, expected) => {
-          // asserting the two objects are equal
-          // e.g. using chai.
-            expect(actual).deep.equal(expected);
-    });
+//https://www.npmjs.com/package/rxjs-marbles
+//https://itnext.io/findings-about-rxjs-marble-testing-and-the-testscheduler-b23c6bdf6b49
 
-it('Simple test RxJS with rsjs-marbles', ()=>{
+it('Simple test RxJS with rsjs-marbles', marbles((helpers)=>{
 
-    testScheduler.run((helpers)=>{
-        const { cold, hot, expectObservable, expectSubscriptions, flush } = helpers;
-
-        const source$=cold('--a--b--c|', {a:5, b:10, c:15});// input test values
-        const expected$=cold('--a--b--c|', {a:10, b:20, c:30});//expected results
+        const source$=helpers.cold('--a--b--c|', {a:5, b:10, c:15});// input test values
+        const expected$=helpers.cold('--a--b--c|', {a:10, b:20, c:30});//expected results
 
 
-        //const result$=source$.pipe(map(x=>x*2));//appling the operator to be tested on the test input values
-        //expect(result$).toBeObservable(expected$)
-
-        expectObservable(source$.pipe(map(x=>x*2))).toBe(expected$);
-        //expectSubscriptions(source$.subscriptions).toBe(subs);
-
-    });
-
-});
+        const result$=source$.pipe(map(x=>x*2));//appling the operator to be tested on the test input values
+        helpers.expect(result$).toBeObservable(expected$)
+}));
